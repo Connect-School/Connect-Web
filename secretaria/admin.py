62be0be27cc60830215fe9d56b0123e009662d6b
RodrigoNdeC <rodrigondec@gmail.com>
2018-04-13 20:35:47 -0300

    secretaria adicionada

--- secretaria/admin.py
+++ secretaria/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from secretaria.models import Secretaria, GerenteSecretaria
 
-# Register your models here.
+
+admin.site.register(Secretaria)
+admin.site.register(GerenteSecretaria)
--- secretaria/models.py
+++ secretaria/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from core.models import Organizacao, Usuario
+from responsabilidade.models import SecretariaResponsavel, SecretariaDependente
 
 # Create your models here.
+class Secretaria(Organizacao):
+    perfil_responsavel = models.OneToOneField(SecretariaResponsavel, related_name="secretaria", null=True, blank=True, on_delete=models.PROTECT)
+    perfil_dependente = models.OneToOneField(SecretariaDependente, related_name="secretaria", null=True, blank=True, on_delete=models.PROTECT)
+    nome = models.CharField(max_length=50)
+    endereco = models.CharField(max_length=50)
+    telefone = models.CharField(max_length=16)
+
+    def get_name(self):
+        return self.nome
+
+    def __str__(self):
+        return self.get_name()
+
+class GerenteSecretaria(Usuario):
+    secretaria = models.ForeignKey(Secretaria, related_name="gerentes", null=True, blank=True, on_delete=models.PROTECT)

4f17efaeba1906d056ba9e9d3cd56b2d6f343341
RodrigoNdeC <rodrigondec@gmail.com>
2018-04-13 18:33:00 -0300

    models adicionados no admin

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from core.models import Usuario, Pai, Aluno
+from core.models import Usuario, Pai, Aluno, Dependente, Responsavel
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Pai)
 admin.site.register(Aluno)
+
+admin.site.register(Dependente)
+admin.site.register(Responsavel)
--- escola/admin.py
+++ escola/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from escola.models import GerenteEscola, Escola
 
 # Register your models here.
+admin.site.register(Escola)
+admin.site.register(GerenteEscola)

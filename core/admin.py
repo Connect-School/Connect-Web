46e309654fffe172fab539be0de5328fc272d67b
RodrigoNdeC <rodrigondec@gmail.com>
2018-04-13 18:30:09 -0300

    Cadastrado users

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Usuario, Pai, Aluno
 
 # Register your models here.
+admin.site.register(Usuario)
+admin.site.register(Pai)
+admin.site.register(Aluno)

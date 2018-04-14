bf008870b51a096ee9b111a75f774d7fcf10fa41
RodrigoNdeC <rodrigondec@gmail.com>
2018-04-14 12:46:12 -0300

    major fix

--- informavel/urls.py
+++ informavel/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('detalhar/forum/<id_informavel>', views.detalhar_forum, name='detalhar_forum'),
-    path('detalhar/aviso/<id_informavel>', views.detalhar_aviso, name='detalhar_aviso'),
+    path('detalhar/aviso/<id_notificacao>', views.detalhar_aviso, name='detalhar_aviso'),
     path('detalhar/bullying/<id_informavel>', views.detalhar_bullying, name='detalhar_bullying'),
 ]
--- informavel/views.py
+++ informavel/views.py
@@ -1,60 +1,57 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from core.models import Gerente
+from .models import Notificacao, Bullying, InformavelForum
 
 
 @login_required
 def detalhar_forum(request, id_informavel):
     context = {}
     context['usuario_autenticado'] = request.user.is_authenticated
-
     usuario = get_object_or_404(Gerente, perfil_user=request.user)
-
     isinstance(usuario, Gerente)
-
     context['organizacao'] = usuario.get_organizacao()
     context['tipo_organizacao'] = usuario.get_organizacao().get_tipo()
 
+    forum = get_object_or_404(InformavelForum, pk=id_informavel)
 
-    context['notificacoes'] = []
-    context['foruns'] = []
+    context['forum'] = forum
+    context['variar'] = True
 
-    return render(request, 'informavel/index.html', context)
+    return render(request, 'informavel/detalhar_forum.html', context)
 
 
 @login_required
-def detalhar_aviso(request, id_informavel):
+def detalhar_aviso(request, id_notificacao):
     context = {}
     context['usuario_autenticado'] = request.user.is_authenticated
-
     usuario = get_object_or_404(Gerente, perfil_user=request.user)
-
     isinstance(usuario, Gerente)
-
     context['organizacao'] = usuario.get_organizacao()
     context['tipo_organizacao'] = usuario.get_organizacao().get_tipo()
 
+    notificacao = get_object_or_404(Notificacao, pk=id_notificacao)
+    notificacao.read = True
+    notificacao.save()
 
-    context['notificacoes'] = []
-    context['foruns'] = []
+    context['aviso'] = notificacao.aviso
 
-    return render(request, 'informavel/index.html', context)
+    return render(request, 'informavel/detalhar_aviso.html', context)
 
 
 @login_required
 def detalhar_bullying(request, id_informavel):
     context = {}
     context['usuario_autenticado'] = request.user.is_authenticated
-
     usuario = get_object_or_404(Gerente, perfil_user=request.user)
-
     isinstance(usuario, Gerente)
-
     context['organizacao'] = usuario.get_organizacao()
     context['tipo_organizacao'] = usuario.get_organizacao().get_tipo()
 
+    bullying = get_object_or_404(Bullying, pk=id_informavel)
+    bullying.read = True
+    bullying.save()
 
-    context['notificacoes'] = []
-    context['foruns'] = []
+    context['bullying'] = bullying
 
-    return render(request, 'informavel/index.html', context)
+    return render(request, 'informavel/detalhar_bullying.html', context)

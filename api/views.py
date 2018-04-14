84119fb360033a16825039a4beacb7993d7fbfcf
RodrigoNdeC <rodrigondec@gmail.com>
2018-04-14 08:09:56 -0300

    api estagnada

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from rest_framework.viewsets import ModelViewSet, ReadOnlyModelViewSet
+from rest_framework.response import Response
 from django.contrib.auth.models import User
 from core.serializers import UserSerializer, UsuarioPolySerializer, OrganizacaoPolySerializer
 from core.models import Usuario, Organizacao
@@ -17,3 +18,13 @@ class UsuarioViewSet(ModelViewSet):
 class OrganizacaoViewSet(ModelViewSet):
     queryset = Organizacao.objects.all()
     serializer_class = OrganizacaoPolySerializer
+
+    def update(self, request, pk=None, project_pk=None):
+        print(request.data['result'])
+
+    def partial_update(self, request, *args, **kwargs):
+        instance = self.queryset.get(pk=kwargs.get('pk'))
+        serializer = self.serializer_class(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
